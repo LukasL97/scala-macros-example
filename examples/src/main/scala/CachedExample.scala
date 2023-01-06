0e6d73a7d41a3a89be42d6b2d28928f42b966b85
Lukas Lehmann <lukas.lehmann97@gmail.com>
2023-01-06 15:22:23 +0100

    generate cache within cached annotation

--- examples/src/main/scala/CachedExample.scala
+++ examples/src/main/scala/CachedExample.scala
@@ -2,14 +2,21 @@ package de.codecentric
 
 object CachedExample extends App {
 
-  val cache = new MapCache[(Int, String), Int]
-
   @cached
   def f(x: Int, y: String): Int = x
+
+  @cached
+  def g(x: Int, y: String): Int = x + 1
+
+  @cached
+  def g(x: Int): Int = x + 1
 
   println(f(1, "test"))
   println(f(1, "test"))
   println(f(2, "test"))
   println(f(2, "rest"))
+  println(g(2, "rest"))
+  println(g(2, "rest"))
+  println(g(2, "rest"))
 
 }
--- macros/src/main/scala/cached.scala
+++ macros/src/main/scala/cached.scala
@@ -15,25 +15,34 @@ object cached {
 
     annottees.head match {
       case q"$mods def $method[..$typeParams](...$params): $returnType = $rhs" =>
+
         val flattenedParams = params.asInstanceOf[Seq[Seq[c.Tree]]].flatten
-        val paramNames = flattenedParams.map {
-          case q"$_ val $name: $_ = $_" => name
-        }
+        val (paramNames, paramTypes) = flattenedParams.map {
+          case q"$_ val $name: $tpt = $_" => name -> tpt
+        }.unzip
+
+        val cacheName = TermName(method + "_generatedCache_" + rhs.pos.hashCode())
+
         val newRhs =
           q"""
            val key = (..$paramNames)
-           return cache.get(key) match {
+           return $cacheName.get(key) match {
              case Some(value) =>
                println("CACHE HIT")
                value
              case None =>
                println("CACHE MISS")
                val result = $rhs
-               cache.put(key, result)
+               $cacheName.put(key, result)
                return result
            }
           """
-        q"$mods def $method[..$typeParams](...$params): $returnType = $newRhs"
+        val expandedMethod = q"$mods def $method[..$typeParams](...$params): $returnType = $newRhs"
+
+        val cacheKeyType = tq"(..$paramTypes)"
+        val cache = q"private val $cacheName: de.codecentric.Cache[$cacheKeyType, $returnType] = new de.codecentric.MapCache[$cacheKeyType, $returnType]"
+
+        q"$expandedMethod; $cache"
       case annottee => c.abort(annottee.pos, "Annottee must be a method")
     }
   }
